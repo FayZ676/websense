d2fd0d0c77a62f7de328aaef1efb27d034bcec51
FayZ676 <faizififita1@gmail.com>
2023-08-30 13:22:31 -0700

    Register and console log tab switching. Build structure for keeping track of focus times.

--- background.js
+++ background.js
@@ -1,10 +1,29 @@
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // "tabs" will either be a "tab.Tab" instance or "undefined"
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
+// console log the url of the current tab
+const tabFocusTimes = {};
 
-// Check for an open tab event
+const calculateFocusTime = (tabId) => {
+  if (tabFocusTimes[tabId]) {
+    const currentTime = new Date().getTime();
+    const focusedTime = currentTime - tabFocusTimes[tabId].timestamp;
+    return focusedTime;
+  }
+  return 0;
+};
 
-// Check for a closed tab event
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    const timestamp = new Date().toLocaleString();
+    const tabId = activeInfo.tabId;
+    const tabUrl = tab.url;
+    const tabTitle = tab.title;
+
+    // Append tabId, url, title, and timestamp to tabFocusTime object
+    tabFocusTime[tabId] = {
+      title: tabTitle,
+      url: tabUrl,
+      timestamp: timestamp,
+    };
+
+    console.log(`[${timestamp}] Active tab: ${tab.url}`);
+  });
+});
