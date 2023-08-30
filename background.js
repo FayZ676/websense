a3ed128c50a796962e970a68ebef85167aa4fc97
FayZ676 <faizififita1@gmail.com>
2023-08-30 15:12:32 -0700

    Succesfully track times for tabs through switching. Need to now display them in the popup.

--- background.js
+++ background.js
@@ -1,29 +1,62 @@
-// console log the url of the current tab
 const tabFocusTimes = {};
 
-const calculateFocusTime = (tabId) => {
-  if (tabFocusTimes[tabId]) {
-    const currentTime = new Date().getTime();
-    const focusedTime = currentTime - tabFocusTimes[tabId].timestamp;
-    return focusedTime;
-  }
-  return 0;
-};
-
+// Fires when the active tab in a window changes.
 chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.get(activeInfo.tabId, (tab) => {
-    const timestamp = new Date().toLocaleString();
-    const tabId = activeInfo.tabId;
-    const tabUrl = tab.url;
-    const tabTitle = tab.title;
+  // Current Tab ID
+  const tabId = activeInfo.tabId;
 
-    // Append tabId, url, title, and timestamp to tabFocusTime object
-    tabFocusTime[tabId] = {
-      title: tabTitle,
-      url: tabUrl,
-      timestamp: timestamp,
-    };
+  // Current Time in milliseconds
+  const timestamp = new Date().getTime();
 
-    console.log(`[${timestamp}] Active tab: ${tab.url}`);
-  });
+  // index of the last active tab (if it exists) in the tabFocusTimes object
+  const lastActiveTab = Object.keys(tabFocusTimes).find(
+    (tabId) => tabFocusTimes[tabId].isActive
+  );
+
+  // if there is a last active tab
+  if (lastActiveTab) {
+    // update the last active tab's endTime
+    tabFocusTimes[lastActiveTab].endTime = timestamp;
+
+    // calculate the focusTimeSeconds of the last active tab
+    tabFocusTimes[lastActiveTab].focusTimeSeconds += Math.round(
+      (tabFocusTimes[lastActiveTab].endTime -
+        tabFocusTimes[lastActiveTab].startTime) /
+        1000
+    );
+
+    // set the last active tab's status to inactive
+    tabFocusTimes[lastActiveTab].isActive = false;
+  }
+
+  // if the current tab is a new one
+  if (!tabFocusTimes[tabId]) {
+    chrome.tabs.get(tabId, (tab) => {
+      //  get the url of the current tab
+      tabUrl = tab.url;
+
+      // get the title of the current tab
+      tabTitle = tab.title;
+
+      // create a new tab object
+      tabFocusTimes[tabId] = {
+        title: tabTitle,
+        url: tabUrl,
+        startTime: timestamp,
+        endTime: null,
+        focusTimeSeconds: 0,
+        isActive: true,
+      };
+    });
+  }
+  // Otherwise if the current tab is not a new one
+  else {
+    // Set the current tab's status to active
+    tabFocusTimes[tabId].isActive = true;
+
+    // Set the current tab's startTime to the current time
+    tabFocusTimes[tabId].startTime = timestamp;
+  }
+
+  console.log(tabFocusTimes);
 });
